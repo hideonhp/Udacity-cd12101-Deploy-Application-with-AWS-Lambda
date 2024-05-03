974f16f7ea8f8d976b7c39f185e871550e85c4c3
HideOnHp <hideonhp@gmail.com>
2024-05-03 17:39:10 +0700

    Create config.ts

---
+++ starter/client/config.ts
@@ -0,0 +1,3 @@
+let REACT_APP_AUTH0_DOMAIN = 'dev-s4ekzwe3wl6g1qm2.us.auth0.com'
+let REACT_APP_AUTH0_CLIENT_ID= 'T9SHiZNQH4GUAAwtkfSpJRKjX08LvD0k'
+let REACT_APP_API_ENDPOINT= 'https://2y15ev0une.execute-api.us-east-1.amazonaws.com/dev'
